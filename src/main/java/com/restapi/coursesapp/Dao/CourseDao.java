e7760d1668cdeca7a6574b80281f8ab7c5b2bd69
Bhaven <chaudharybhaven@gmail.com>
2022-05-04 00:02:25 +0530

    final commit

--- src/main/java/com/restapi/coursesapp/Dao/CourseDao.java
+++
@@ -1,9 +0,0 @@
-package com.restapi.coursesapp.Dao;
-
-import com.restapi.coursesapp.Entities.Course;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseDao extends JpaRepository<Course, Long> {
-
-}
